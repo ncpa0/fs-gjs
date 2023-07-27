b2c421f21dcaf1812d7399c30809baf974ce0f35
ncpa0cpl <sbretner@touchbistro.com>
2023-07-27 12:09:03 +0200

    ci: fix publish script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -7,9 +7,6 @@ set -o errexit
 issemver=$(./scripts/check-semver.sh -t "$TAG_NAME")
 currenttag=$(npm pkg get version)
 
-git config user.name github-actions
-git config user.email github-actions@github.com
-
 if [ "$issemver" -eq "1" ]; then
     if ! [ "$currenttag" = "\"$TAG_NAME\"" ]; then
         echo "Branch tag is different than packge.json version. Updating package.json version to $TAG_NAME"
